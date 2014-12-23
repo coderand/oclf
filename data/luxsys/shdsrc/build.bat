3c8831e8df81aa56175a3ac1b2c1869b6b64ece8
Dmitry Andreev <andcoder@gmail.com>
2014-12-22 23:06:57 -0800

    added data

---
+++ data/luxsys/shdsrc/build.bat
@@ -0,0 +1,15 @@
+psa ocglowout.psa
+psa ocblur.psa
+vsa ocblur.vsa
+psa amhbump.psa
+vsa amhbump.vsa
+psa octile.psa
+vsa octile.vsa
+
+move ocglowout.pso ../ocglowout.pso
+move ocblur.pso ../ocblur.pso
+move ocblur.vso ../ocblur.vso
+move amhbump.pso ../amhbump.pso
+move amhbump.vso ../amhbump.vso
+move octile.pso ../octile.pso
+move octile.vso ../octile.vso
